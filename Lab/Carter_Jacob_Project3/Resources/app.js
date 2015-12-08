f76eebec9951bac560643656f2a2c3d01bec6263
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-12-08 14:26:32 -0500

    Lab 5 i think progress
    progress..

--- Lab/Carter_Jacob_Project3/Resources/app.js
+++ Lab/Carter_Jacob_Project3/Resources/app.js
@@ -1,12 +1,16 @@
+//images
+var galleryFolder = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,"images");
+var gallery = galleryFolder.getDirectoryListing();
+var swipe = require("swipe");
+
 Ti.UI.setBackgroundColor("fff");
 var win = Ti.UI.createWindow({
 	backgroundImage: "background.jpg",
 	top: 20
 });
 
-
 //ScrollView
-var viewContainer = Ti.UI.createScrollView({
+var scrollView = Ti.UI.createScrollView({
 	layout: "horizontal",
 	contentWidth: 300,
 	top: 28,
@@ -14,25 +18,24 @@ var viewContainer = Ti.UI.createScrollView({
 });
 
 
-//images
-var galleryFolder = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,"images");
-var gallery = galleryFolder.getDirectoryListing();
 
 
 //bottom, left here and top in scrollview are the same so there is even empty space everywhere!
 for(var i=0; i<gallery.length; i++){
 	var images = Ti.UI.createImageView({
 		image: "images/" + gallery[i],
-		width:320,
-		height: 180,
+		width:160,
+		height: 90,
 		bottom: 28,
-		left: 28,
+		//left: 28,
 		borderColor: "#fff",
 		borderWidth: 1
 	});
-	viewContainer.add(images);
+	scrollView.add(images);
 }
 
-
-win.add(viewContainer);
+scrollView.addEventListener("click", function(){
+	swipe.swipeWin.open();
+});
+win.add(scrollView);
 win.open();
