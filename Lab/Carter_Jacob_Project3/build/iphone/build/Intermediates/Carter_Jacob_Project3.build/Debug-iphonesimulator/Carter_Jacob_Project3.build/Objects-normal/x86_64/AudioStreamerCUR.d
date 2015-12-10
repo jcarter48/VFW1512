aa0b34a7d7a23ef6cb71da3f13df1b582ac41176
jcarter48 <jacobcarter@Jacobs-MacBook-Pro.local>
2015-12-10 15:18:30 -0500

    Project 3 Done
    also fixed/renamed previous projects

---
+++ Lab/Carter_Jacob_Project3/build/iphone/build/Intermediates/Carter_Jacob_Project3.build/Debug-iphonesimulator/Carter_Jacob_Project3.build/Objects-normal/x86_64/AudioStreamerCUR.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jacobcarter/Documents/VFW1512/Lab/Carter_Jacob_Project3/build/iphone/Classes/AudioStreamer/AudioStreamerCUR.m \
+  /Users/jacobcarter/Documents/VFW1512/Lab/Carter_Jacob_Project3/build/iphone/Classes/AudioStreamer/AudioStreamerCUR.h \
+  /Users/jacobcarter/Documents/VFW1512/Lab/Carter_Jacob_Project3/build/iphone/Classes/AudioStreamer/AudioStreamer.h
---
+++ Lab/Carter_Jacob_Project3/build/iphone/main.m
@@ -0,0 +1,40 @@
+//
+//  Appcelerator Titanium Mobile
+//  WARNING: this is a generated file and should not be modified
+//
+
+#import <UIKit/UIKit.h>
+#define _QUOTEME(x) #x
+#define STRING(x) _QUOTEME(x)
+
+NSString * const TI_APPLICATION_DEPLOYTYPE = @"development";
+NSString * const TI_APPLICATION_ID = @"com.carter.project3";
+NSString * const TI_APPLICATION_PUBLISHER = @"jacobcarter";
+NSString * const TI_APPLICATION_URL = @"";
+NSString * const TI_APPLICATION_NAME = @"Carter_Jacob_Project3";
+NSString * const TI_APPLICATION_VERSION = @"1.0";
+NSString * const TI_APPLICATION_DESCRIPTION = @"";
+NSString * const TI_APPLICATION_COPYRIGHT = @"2015 by jacobcarter";
+NSString * const TI_APPLICATION_GUID = @"a2f0b3d3-181c-4195-9999-b888be4fe79c";
+BOOL const TI_APPLICATION_ANALYTICS = true;
+NSString * const TI_APPLICATION_BUILD_TYPE = @"";
+
+#ifdef TARGET_IPHONE_SIMULATOR
+NSString * const TI_APPLICATION_RESOURCE_DIR = @"";
+#endif
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+#ifdef __LOG__ID__
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectory = [paths objectAtIndex:0];
+	NSString *logPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%s.log",STRING(__LOG__ID__)]];
+	freopen([logPath cStringUsingEncoding:NSUTF8StringEncoding],"w+",stderr);
+	fprintf(stderr,"[INFO] Application started\n");
+#endif
+
+	int retVal = UIApplicationMain(argc, argv, nil, @"TiApp");
+    [pool release];
+    return retVal;
+}
